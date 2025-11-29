ed7a2ce4444b9e73b58c44cc7643fc90d37cd86d
syaofox <syaofox@gmail.com>
2025-11-30 07:55:40 +0800

    Update picom opacity rules and Thunar configuration for improved usability and functionality

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -113,6 +113,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *appfindercmd[] = { "/usr/bin/xfce4-appfinder", NULL };
+static const char *roficmd[] = { "/bin/sh", "-c", "rofi -show drun -theme $HOME/.config/mint-dwm/config/rofi-theme.rasi", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray3, NULL };
 static const char *termcmd[]  = { "/usr/bin/x-terminal-emulator",  NULL };
 static const char *filecmd[]  = { "/usr/bin/pcmanfm",  NULL };
@@ -142,8 +143,7 @@ static const AppKey appkeys[] = {
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = appfindercmd } }, /* 启动菜单 */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, /* 启动菜单 */
+	{ MODKEY,                       XK_space,  spawn,          {.v = roficmd } }, /* 启动菜单 */
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } }, /* 启动终端 */
     { MODKEY,                       XK_e,      spawn,          {.v = filecmd } }, /* 启动thunar */
     { MODKEY,                       XK_a,      spawn,          {.v = screenshotcmd } }, /* 截图 */
