13f1b764e479459907b079ceb6ea4d9dca70afe8
syaofox <syaofox@gmail.com>
2025-11-30 09:41:32 +0800

    Update terminal launcher in run-term.sh to prioritize 'st' over 'alacritty', enhancing terminal selection logic.

--- scripts/run-term.sh
+++ scripts/run-term.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if command -v alacritty >/dev/null 2>&1; then
+if command -v st >/dev/null 2>&1; then
+    exec st "$@"
+elif command -v alacritty >/dev/null 2>&1; then
     exec alacritty --config-file "$HOME/.config/mint-dwm/config/alacritty.toml" "$@"
 else
     exec /usr/bin/x-terminal-emulator "$@"
