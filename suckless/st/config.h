3c850f81991c9d0c4c7e91910372a3309b40b0a7
syaofox <syaofox@gmail.com>
2025-11-30 09:40:27 +0800

    Update terminal color scheme in st configuration, replacing default colors with a new palette for enhanced visual aesthetics and consistency.

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -114,46 +114,45 @@ const int boxdraw_braille = 0;
  /* bg opacity */
  float alpha = 0.8;
  
- /* Terminal colors (16 first used in escape sequence) */
- static const char *colorname[] = {
-	 /* 8 normal colors */
-	 "black",
-	 "red3",
-	 "green3",
-	 "yellow3",
-	 "blue2",
-	 "magenta3",
-	 "cyan3",
-	 "gray90",
- 
-	 /* 8 bright colors */
-	 "gray50",
-	 "red",
-	 "green",
-	 "yellow",
-	 "#5c5cff",
-	 "magenta",
-	 "cyan",
-	 "white",
- 
-	 [255] = 0,
- 
-	 /* more colors can be added after 255 to use with DefaultXX */
-	 "#cccccc",
-	 "#555555",
-	 "gray90", /* default foreground colour */
-	 "black", /* default background colour */
- };
- 
- 
- /*
-  * Default colors (colorname index)
-  * foreground, background, cursor, reverse cursor
-  */
- unsigned int defaultfg = 258;
- unsigned int defaultbg = 259;
- unsigned int defaultcs = 256;
- static unsigned int defaultrcs = 257;
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#0d1416",
+	"#ff5555",
+	"#50fa7b",
+	"#f1fa8c",
+	"#bd93f9",
+	"#ff79c6",
+	"#60DEEC",
+	"#bbbbbb",
+
+	/* 8 bright colors */
+	"#44475a",
+	"#ff5555",
+	"#50fa7b",
+	"#f1fa8c",
+	"#bd93f9",
+	"#ff79c6",
+	"#60DEEC",
+	"#ffffff",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#0d1416", /* 256: background */
+	"#c5d9dc", /* 257: foreground */
+	"#60DEEC", /* 258: cursor */
+};
+
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 256;
  
  /*
   * Default shape of cursor
