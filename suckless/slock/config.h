18b37c5c7a4dbfd72a3647629dd4ebb074d0ba8d
syaofox <syaofox@gmail.com>
2025-11-30 11:22:54 +0800

    patch slock-message-20191002-b46028b

--- suckless/slock/config.h
+++ suckless/slock/config.h
@@ -3,10 +3,19 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#0d1416",     /* after initialization */
-	[INPUT] =  "#60DEEC",   /* during input */
+	[INIT] =   "#587279",     /* after initialization */
+	[INPUT] =  "#3DB0BD",   /* during input */
 	[FAILED] = "#ff7f70",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Simple is simple, double is double.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "10x20";
