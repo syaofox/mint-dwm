4dd053553e230cbd44e1ae622ea26d080837347b
syaofox <syaofox@gmail.com>
2025-11-30 09:28:14 +0800

    patch font2

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
- static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+ static char *font = "JetBrainsMono Nerd Font Propo:style=Bold:size=14:pixelsize=14:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
  static int borderpx = 2;
  
  /*
