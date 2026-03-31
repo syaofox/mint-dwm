68056a6deadd0325ce071a70e8b5b9e1641981c4
syaofox <syaofox@gmail.com>
2026-03-31 20:38:53 +0800

    feat: 启用 picom 组合器并优化 rofi 显示布局
    变更内容:
    - 启用 picom 窗口组合器，优化窗口渲染效果
    - 优化 rofi 启动器布局为 4 列，窗口大小调整为 40%x60%

--- scripts/run-launcher.sh
+++ scripts/run-launcher.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 if command -v rofi >/dev/null 2>&1; then
-    exec rofi -show drun -theme "$HOME/.config/mint-dwm/config/rofi-theme.rasi"
+    exec rofi -show drun -theme "$HOME/.config/mint-dwm/config/rofi-theme.rasi" -theme-str "listview { columns: 4; } window { width: 40%; height: 60%; }"
 else
     exec /usr/bin/xfce4-appfinder
 fi
-
